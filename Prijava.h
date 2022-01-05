664646f01ed025a6703584176017d7275a0fdfe9
TheLazyTownie <adodik28@gmail.com>
2022-01-05 22:44:36 +0100

    Prvi commit
    main funkcija, header fajl za prijavu

---
+++ Prijava.h
@@ -0,0 +1,11 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include<fstream>
+
+void prijava();
+int provjeriNalog(std::string&, std::string&);
+void printMeniSef();
+void printMeniRadnik();
+void promjenaSifre(std::string&, int);
+void povecajBrojPrijava(std::string&);
