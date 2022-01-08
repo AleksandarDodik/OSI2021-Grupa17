a1e7009f67fb45dfa24d1ab093ada80be56094ee
David Bursac <davidbursac68@gmail.com>
2022-01-08 22:29:26 +0100

    Brojanje prijava, promjena sifre, izborni meni

--- Prijava.cpp
+++ Prijava.cpp
@@ -84,3 +84,133 @@ int provjeriNalog(std::string& ime, std::string& sifra)
 	return -1;
 
 }
+void printMeniSef()
+{
+	int a;
+	Artikal art;
+	system("cls");
+	std::cout << "-----------------------\n";
+	std::cout << "1. Pregled artikala\n" << "2. Dodavanje artikla\n" << "3. Brisanje artikla\n" << "4. Izvjestaj o prodaji\n\n" << "5. Kreiranje naloga\n" << "6. Brisanje naloga\n\n" << "7. Odjavi se\n" << "8. Zatvori aplikaciju\n";
+	std::cout << "-----------------------\n";
+	do {
+		std::cout << "Unesite broj: ";
+		std::cin.sync();
+		std::cin >> a;
+		std::cin.get();//Da pokupi enter
+	} while (a < 1 || a>8);
+	switch (a)
+	{
+	case 1:
+		art.pregledArtikala();
+		break;
+	case 2:
+		art.dodavanjeArtikla();
+		break;
+	case 3:
+		art.obrisiArtikl();
+		break;
+	case 4:
+		art.izvjestaj();
+		break;
+	case 5:
+		Registracija();
+		break;
+	case 6:
+		obrisiNalog();
+		break;
+	case 7:
+		prijava();
+		break;
+	case 8:
+		break;
+	}
+}
+
+void printMeniRadnik()
+{
+	int a;
+	Racun r;
+	system("cls");
+	std::cout << "-----------------------\n";
+	std::cout << "1. Kreiraj novi racun\n\n" << "2. Odjavi se\n" << "3.Zatvori aplikaciju\n";
+	std::cout << "-----------------------\n";
+	do {
+		std::cout << "Unesite broj: ";
+		std::cin.sync();
+		std::cin >> a;
+		std::cin.get();//Da pokupi enter
+	} while (a < 1 || a>3);
+	switch (a)
+	{
+	case 1:
+		r.dodavanjeArtikala();
+		break;
+	case 2:
+		prijava();
+		break;
+	case 3:
+		break;
+	}
+}
+
+void promjenaSifre(std::string& Ime, int tip)
+{
+	system("cls");
+	std::cout << "Promjena sifre za nalog: " << Ime << std::endl;
+
+	std::string novaSifra, ponovljenaSifra;
+	do {
+		std::cout << "\tNova sifra: ";
+		std::getline(std::cin, novaSifra);
+
+		std::cout << "\tPonvoljena nova sifra: ";
+		std::getline(std::cin, ponovljenaSifra);
+		if (novaSifra.compare(ponovljenaSifra) != 0)
+			std::cout << "Sifre su razlicite!\n";
+	} while (novaSifra.compare(ponovljenaSifra) != 0);
+
+	brisanjeNaloga(Ime);
+
+	std::ofstream BazaNaloga;
+	BazaNaloga.open("nalozi.csv", std::ios::app);
+	if (BazaNaloga.is_open())
+	{
+		BazaNaloga << Ime << ',' << novaSifra << ',' << ((tip == 10) ? 's' : 'r') << ',' << 0 << '\n';
+		BazaNaloga.close();
+	}
+
+
+}
+
+void povecajBrojPrijava(std::string& Ime)
+{
+	std::string _ime, _sifra, _tip, _brojPrijava;
+	//Otvaranje Temp za citanje
+	std::ifstream BazaNaloga;
+	BazaNaloga.open("nalozi.csv");
+
+	if (BazaNaloga.is_open())
+	{
+		while (!BazaNaloga.eof())
+		{
+			getline(BazaNaloga, _ime, ',');
+			getline(BazaNaloga, _sifra, ',');
+			getline(BazaNaloga, _tip, ',');
+			getline(BazaNaloga, _brojPrijava);
+			if (_ime.compare(Ime) == 0)
+				break;
+		}
+		BazaNaloga.close();
+	}
+
+	brisanjeNaloga(_ime);
+
+	std::ofstream Temp;
+	Temp.open("nalozi.csv", std::ios::app);
+	if (Temp.is_open())
+	{
+		Temp << _ime << ',' << _sifra << ',' << _tip << ',' << stoi(_brojPrijava) + 1 << '\n';
+		Temp.close();
+	}
+
+}
